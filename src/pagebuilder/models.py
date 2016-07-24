1e1f3be4b96294a02904fc6168619aff95f50f36
coblan <coblan@163.com>
2016-07-25 01:19:45 +0800

    builer

--- src/pagebuilder/models.py
+++ src/pagebuilder/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class PageModel(models.Model):
+    name = models.CharField('name',max_length=300,blank=True)
+    label = models.CharField('label',max_length=400,blank=True)
+    content = models.TextField(verbose_name='content',blank=True)
