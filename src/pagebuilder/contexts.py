b79ab3bc6c3dae4e0b5c7d160f6e46bae7589c55
coblan <coblan@163.com>
2016-09-01 01:00:40 +0800

    dg

---
+++ src/pagebuilder/contexts.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render,Http404
+from core.db_tools import form_to_head,to_dict
+from models import MobilePage
+from forms import MobilePageForm
+import json
+
+def get_cls(step):
+    name = 'MB%s%s'%(step[0].capitalize(),step[1:].lower())
+    return globals().get(name)
+
+class Object(object):
+    pass
+
+class MBBase(object):
+    template=''
+    def __init__(self):
+        self.ctx=Object()
+    
+    def get_dict(self):
+        return self.ctx.__dict__
+    
+    def get(self,request,step):
+        self.step = step
+        self.build(request)
+        return render(request,self.template,context=self.get_dict())
+    
+    def build(self,request):
+        self.ctx.menu_active = 'content'
+        self.ctx.submenu_active=self.step
+
+class MBPage(MBBase):
+    template='mobile/mbpage.html'
+    
+    def build(self,request):
+        MBBase.build(self,request)
+        self.ctx.heads = json.dumps(form_to_head( MobilePageForm())) 
+        self.ctx.pages = json.dumps([to_dict(x) for x in MobilePage.objects.all()] )
+
+class MBMenu(MBBase):
+    template = 'mobile/mbmenu.html'
+          
--- src/pagebuilder/views.py
+++ src/pagebuilder/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,Http404
 from core.port import jsonpost
 import ajax
 import json
 from forms import MobilePageForm
 from core.db_tools import form_to_head,to_dict
 from models import MobilePage
+import contexts
 # Create your views here.
 def build(request):
     return render(request,'builder.html')
@@ -16,8 +17,14 @@ def pagebuild(request):
     else:
         return jsonpost(request, ajax.get_globle())
 
-def mobile_page(request):
+def mobile_page(request,step='page'):
+    
     if request.method=='GET':
+        cls = contexts.get_cls(step)
+        if cls:
+            return cls().get(request,step)
+        else:
+            raise Http404('page not found')
         context = {'heads':json.dumps(form_to_head( MobilePageForm()) ),
                    'pages':json.dumps([to_dict(x) for x in MobilePage.objects.all()])
                    }
