46e9678474de9510941b61052ea193784ec67f15
coblan <coblan@163.com>
2018-12-06 23:04:36 +0800

    Update server.py

--- src/settings/server.py
+++ src/settings/server.py
@@ -25,7 +25,7 @@ DATABASES = {
         'NAME': 'first',
         'ENGINE': 'django.db.backends.mysql',
         'USER': 'root',
-        'PASSWORD': 'he123811',
+        'PASSWORD': 'root553',
         'HOST': '127.0.0.1', 
         'PORT': '3306',        
       },
